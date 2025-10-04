87d216da958d20969f28af43451d3f848b38b178
PhatDos <162654433+PhatDos@users.noreply.github.com>
2025-10-04 21:20:51 +0700

    style: message file modal

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -36,7 +36,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
 
   if (url && type.includes("pdf")) {
     return (
-      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+      <div className="relative flex flex-col items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
         <a
           href={url}
@@ -44,7 +44,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
           rel="noopener noreferrer"
           className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
         >
-          {url}
+          File PDF
         </a>
         <button
           onClick={() => onChange(undefined)}
