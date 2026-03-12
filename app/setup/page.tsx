072d9a77c34d39dcf9137d8d8809e9b4964f0e51
PhatDos <162654433+PhatDos@users.noreply.github.com>
2026-03-12 16:37:40 +0700

    fix: handle case when user is not logged in in initialProfile()

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -1,8 +1,11 @@
 import { InitialModal } from "@/components/modals/initial-modal";
 import { redirect } from "next/navigation";
 import { getInitialServer } from "@/services/servers/servers-ssr-service";
+import { initialProfile } from "@/lib/intial-profile";
 
 const SetupPage = async () => {
+  await initialProfile();
+
   const server = await getInitialServer();
 
   if (server) {
